e6f8c542a0ee62450bdb99cd0dfa76e9646e3a16
cdp21d <cdp21d@fsu.edu>
2023-04-18 09:51:39 -0400

    a5

--- skillsets/Q13_Sphere_Volume_Calculator/Main.java
+++ skillsets/Q13_Sphere_Volume_Calculator/Main.java
@@ -3,5 +3,6 @@ class Main
     public static void main(String args[])
     {
         Methods.getRequirements();
+        Methods.getSphereVolume();
     }
 }
--- skillsets/Q13_Sphere_Volume_Calculator/Methods.java
+++ skillsets/Q13_Sphere_Volume_Calculator/Methods.java
@@ -12,35 +12,35 @@ public class Methods
         System.out.println("Program checks for non-integers and non-numeric values.");
         System.out.println("Program continues to prompt for user entry until no longer requested, prompt accepts upper of lower case letters.");
         System.out.println();
-
-        System.out.println("Please enter diameter in inches: a");
-        System.out.println("Not valid integer!");
-        System.out.println();
-
-
-        System.out.println("Please try again. Enter diameter in inches: 1.5");
-        System.out.println("Not valid integer!");
-
-        System.out.println();
-
-        System.out.println("Please try again. Enter diameter in inches: 10");
-        System.out.println();
-
-        System.out.println("Sphere volume: 2.27 liquid U.S. gallons");
-
-        System.out.println();
-
-        System.out.println("Do vou want to calculate another sphere volume (v or n)? Y");
-        System.out.println("Please enter diameter in inches: 20");
-
-        System.out.println();
-
-        System.out.println("Sphere volume: 18.13 liquid U.S. gallons");
-
-        System.out.println();
-
-        System.out.println("Do you want to calculate another sphere volume (y or n)? n");
-        System.out.println();
-        System.out.println("Thank you for using our Sphere Volume Calculator!");
     }
+    public static void getSphereVolume()
+    {
+        int diameter = 0;
+        double volume = 0.0;
+        public static void getSphereVolume()
+double gallons = 0.0;
+char choice = ''; //initialize to space character
+Scanner sc = new Scanner(System.in);
+//Note: do...while will *always* execute at least *once*!
+do
+{
+System.out.print("Please enter diameter in inches: ");
+while (!sc.hasNextInt())
+{
+System.out.println("Not valid integer!\n");
+sc.next();//Important! If omitted, will go into infinite loop on invalid input!
+System.out.print("Please try again. Enter diameter in inches: ");
 }
+diameter = sc.nextInt();
+System.out.println(); // print blank line
+//Note: *must* use floating point values, otherwise, integer division (i.e., incorrect result)!
+volume = ((4.0/3.0) * Math. PI * Math.pow(diameter/2.0, 3)); // returns cubic inches
+gallons = volume/231; //convert cubic inches into gallons
+System.out. printin("Sphere volume: " + String. format(™*,.2P", gallons) + " liquid U.S. gallons");
+System.out. print nDo you want to calculate another sphere volume (y or n)? ");
+choice = sc.next() charAt(0);
+choice = Character. toLowerCase(choice);
+}
+while (choice == 'y');
+System.out. printin("\nThank you for using our Sphere Volume Calculator!");
+    }
